202946b0c063a46f832015cdd06fd26205cf33e0
Jake <llira09@gmail.com>
2020-08-18 18:47:46 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,9 @@
+def greeting 
+  puts "Hello World"
+end
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Jake", "Javascript")
---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+name = "Jake"
+#name = "Luca"
+
+puts "We're sorrr,but per health inspector's rules, #{name} is not alllowed in the store"
